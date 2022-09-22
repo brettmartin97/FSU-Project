b58fb72c12de61b25376270752ca064e50bc14b5
HaydenN06 <haydennichols00@gmail.com>
2022-09-21 22:29:44 -0500

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -3,17 +3,17 @@ CREATE DATABASE if not exists fsu;
 use fsu;
 
 CREATE TABLE if not exists `user` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `userId` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `phone` int unsigned NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `role` INT unsigned NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-insert  into `user`(`id`,`name`,`email`,`phone`,`username`,`password`,`role`) values
+insert  into `user`(`userId`,`name`,`email`,`phone`,`username`,`password`,`role`) values
 (1,'Brett Martin','brettdrew@gmail.com',2147483647,'bmartin','$2b$12$nWfHVJ/lyS8HxtL6Q6953.EoV79MMjQn4hegWhxEXka6lWb9CJF0C',13);
 
 
