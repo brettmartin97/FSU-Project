983ca0a129bd84404ad8fc2c5e03492c513e98db
Ibis22 <79343516+Ibis22@users.noreply.github.com>
2022-10-08 15:57:13 -0500

    add three functions to sql.py
    Added three functions get_user_info, get_email, get_id. The get_user_info retrieves all info about a user and returns it all in a list.

--- app/utils/sql.py
+++ app/utils/sql.py
@@ -88,3 +88,74 @@ def get_name(user, log):
     conn.close()
     
     return firstName, lastName
+
+
+
+
+def get_user_info(user):
+    query = f'SELECT * FROM user WHERE username = "{user}"'
+
+    conn = pymysql.connect(host='db',
+                           user='root',
+                           password="root",
+                           db='fsu')
+
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    cursor.execute(query)
+
+    sqlInfo = cursor.fetchone()
+    info = list()
+
+    info.append(sqlInfo['userID'])
+    info.append(sqlInfo['firstName'])
+    info.append(sqlInfo['lastName'])
+    info.append(sqlInfo['email'])
+    info.append(sqlInfo['phone'])
+    info.append(sqlInfo['username'])
+    info.append(sqlInfo['role'])
+
+    conn.close()
+    print(info)
+
+    return info
+
+
+def get_email(user):
+    query = f'SELECT email FROM user WHERE username = "{user}"'
+
+    conn = pymysql.connect(host='db',
+        user='root', 
+        password = "root",
+        db='fsu')
+
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    
+    cursor.execute(query)
+
+    email = cursor.fetchone()['email']
+
+    conn.close()
+    print(email)
+
+    return email
+
+def get_id(user):
+    query = f'SELECT usrID FROM user WHERE username = "{user}"'
+
+    conn = pymysql.connect(host='db',
+        user='root', 
+        password = "root",
+        db='fsu')
+
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    
+    cursor.execute(query)
+
+    id = cursor.fetchone()['id']
+
+    conn.close()
+
+    print(id)
+
+    return id
