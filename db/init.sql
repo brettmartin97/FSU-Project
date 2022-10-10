2b02daed117798ef1685367e468fb8ce8ab4ae5a
HaydenN06 <haydennichols00@gmail.com>
2022-10-10 12:33:47 -0500

    Fixed the Database
    role is now roleId.

--- db/init.sql
+++ db/init.sql
@@ -2,6 +2,14 @@ CREATE DATABASE if not exists fsu;
 
 use fsu;
 
+-- Updating tables in database.
+CREATE TABLE if not exists `Role` (
+  `roleId` int unsigned NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(50) NOT NULL,
+  `commission` varchar(30),
+  PRIMARY KEY (`roleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 CREATE TABLE if not exists `User` (
   `userId` int unsigned NOT NULL AUTO_INCREMENT,
   `firstName` varchar(50) NOT NULL,
@@ -10,35 +18,18 @@ CREATE TABLE if not exists `User` (
   `phone` int unsigned NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `role` INT unsigned NOT NULL,
-  PRIMARY KEY (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- New Tables:
-
-CREATE TABLE if not exists `StylistLevel` (
-  `levelId` int unsigned NOT NULL AUTO_INCREMENT,
-  `levelName` varchar(50) NOT NULL,
-  PRIMARY KEY (`levelId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE if not exists `Stylist` (
-  `stylistId` int unsigned NOT NULL AUTO_INCREMENT,
-  `userId` int unsigned NOT NULL,
-  `levelId` int unsigned NOT NULL,
-  `booth` varchar(20) NOT NULL,
-  PRIMARY KEY (`stylistId`),
-  FOREIGN KEY (`userId`) REFERENCES `User` (`userId`),
-  FOREIGN KEY (`levelId`) REFERENCES `StylistLevel` (`levelId`)
+  `roleId` INT unsigned NOT NULL,
+  PRIMARY KEY (`userId`),
+  FOREIGN KEY (`roleId`) REFERENCES `Role` (`roleId`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE if not exists `Schedule` (
   `dayId` int unsigned NOT NULL,
-  `stylistId` int unsigned NOT NULL,
+  `userId` int unsigned NOT NULL,
   `startTime` time NOT NULL,
   `endTime` time NOT NULL,
-  PRIMARY KEY (`dayId`, `stylistId`),
-  FOREIGN KEY (`stylistId`) REFERENCES `Stylist` (`stylistId`) ON DELETE CASCADE
+  PRIMARY KEY (`dayId`, `userId`),
+  FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE if not exists `AppointmentType` (
@@ -52,11 +43,11 @@ CREATE TABLE if not exists `AppointmentType` (
 CREATE TABLE if not exists `Pricing` (
   `totalPriceId` int unsigned NOT NULL AUTO_INCREMENT,
   `appointTypeId` int unsigned NOT NULL,
-  `levelId` int unsigned NOT NULL,
+  `roleId` int unsigned NOT NULL,
   `price` varchar(100) NOT NULL,
-  PRIMARY KEY (`totalPriceId`, `appointTypeId`, `levelId`),
+  PRIMARY KEY (`totalPriceId`, `appointTypeId`, `roleId`),
   FOREIGN KEY (`appointTypeId`) REFERENCES `AppointmentType` (`appointTypeId`) ON DELETE CASCADE,
-  FOREIGN KEY (`levelId`) REFERENCES `StylistLevel` (`levelId`) ON DELETE CASCADE
+  FOREIGN KEY (`roleId`) REFERENCES `Role` (`roleId`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE if not exists `Customer` (
@@ -70,14 +61,14 @@ CREATE TABLE if not exists `Customer` (
 
 CREATE TABLE if not exists `Appointment` (
   `appointId` int unsigned NOT NULL AUTO_INCREMENT,
-  `stylistId` int unsigned NOT NULL,
+  `userId` int unsigned NOT NULL,
   `appointTypeId` int unsigned NOT NULL,
   `customerId` int unsigned,
   `totalPriceId` int unsigned,
   `notes` varchar(250),
   `startTime` datetime NOT NULL,
   PRIMARY KEY (`appointId`),
-  FOREIGN KEY (`stylistId`) REFERENCES `Stylist` (`stylistId`) ON DELETE CASCADE,
+  FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE,
   FOREIGN KEY (`appointTypeId`) REFERENCES `AppointmentType` (`appointTypeId`) ON DELETE CASCADE,
   FOREIGN KEY (`customerId`) REFERENCES `Customer` (`customerId`) ON DELETE CASCADE,
   FOREIGN KEY (`totalPriceId`) REFERENCES `Pricing` (`totalPriceId`) ON DELETE CASCADE
@@ -85,8 +76,25 @@ CREATE TABLE if not exists `Appointment` (
 
 -- Inserting dummy test data:
 
--- Insert data into user table.
-insert  into `User`(`userId`,`firstName`,`lastName`,`email`,`phone`,`username`,`password`,`role`) values
+-- Insert data into the Role table.
+insert  into `Role`(`roleId`,`roleName`, `commission`) values
+(1, 'Level 1 Stylist', '5%'),
+(2, 'Level 2 Stylist', '10%'),
+(3, 'Level 3 Stylist', '15%'),
+(4, 'Level 4 Stylist', '20%'),
+(5, 'Level 5 Stylist', '25%'),
+(6, 'Level 6 Stylist', '30%'),
+(7, 'Level 7 Stylist', '30%'),
+(8, 'Level 8 Stylist', '30%'),
+(9, 'Level 9 Stylist', '30%'),
+(10, 'Level 10 Stylist', '30%'),
+(11, 'Booth Stylist', '35%'),
+(12, 'Deskworker', 'N\A'),
+(13, 'Admin', 'N\A');
+
+
+-- Insert data into the User table.
+insert  into `User`(`userId`,`firstName`,`lastName`,`email`,`phone`,`username`,`password`,`roleId`) values
 (1,'Admin', 'User','brettdrew@gmail.com',2147483647,'admin','$2b$12$nWfHVJ/lyS8HxtL6Q6953.EoV79MMjQn4hegWhxEXka6lWb9CJF0C',13),
 (2,'Alex', 'Temp1','temp1@gmail.com',111222333,'alex','$2b$12$nWfHVJ/lyS8HxtL6Q6953.EoV79MMjQn4hegWhxEXka6lWb9CJF0C', 1),
 (3,'Bella', 'Temp2','temp2@gmail.com',444555666,'bella','$2b$12$nWfHVJ/lyS8HxtL6Q6953.EoV79MMjQn4hegWhxEXka6lWb9CJF0C', 2),
@@ -101,35 +109,9 @@ insert  into `User`(`userId`,`firstName`,`lastName`,`email`,`phone`,`username`,`
 (12,'Katherine', 'Temp11','temp11@gmail.com',777222555,'katherine','$2b$12$nWfHVJ/lyS8HxtL6Q6953.EoV79MMjQn4hegWhxEXka6lWb9CJF0C', 11),
 (13,'Lily', 'Temp12','temp12@gmail.com',999777111,'lily','$2b$12$nWfHVJ/lyS8HxtL6Q6953.EoV79MMjQn4hegWhxEXka6lWb9CJF0C', 12);
 
--- Insert data into StylistLevel table.
-insert  into `StylistLevel`(`levelId`,`levelName`) values
-(1, 'Level 1'),
-(2, 'Level 2'),
-(3, 'Level 3'),
-(4, 'Level 4'),
-(5, 'Level 5'),
-(6, 'Level 6'),
-(7, 'Level 7'),
-(8, 'Level 8'),
-(9, 'Level 9'),
-(10, 'Level 10'),
-(11, 'Booth Stylist');
 
--- Insert data into Stylist table.
-insert  into `Stylist`(`stylistId`,`userId`,`levelId`,`booth`) values
-(1, 2, 1,'N\A'),
-(2, 3, 2,'N\A'),
-(3, 4, 3,'N\A'),
-(4, 5, 4,'N\A'),
-(5, 6, 5,'N\A'),
-(6, 7, 6,'N\A'),
-(7, 8, 7,'N\A'),
-(8, 9, 8,'N\A'),
-(9, 10, 9,'N\A'),
-(10, 11, 10,'N\A'),
-(11, 12, 11,'Booth 1');
 
--- Insert data into Customer table.
+-- Insert data into the Customer table.
 insert  into `Customer`(`customerId`,`firstName`,`lastName`,`email`,`phoneNumber`) values
 (1,'Austin', 'Custemp1','custemp1@gmail.com', 850111222),
 (2,'Brian', 'Custemp2','custemp2@gmail.com', 850222111),
