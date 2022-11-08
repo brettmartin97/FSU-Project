c29d3a39ba5040d4a0ac5a3d655397e52422608f
Brett Martin <brettdrew@gmail.com>
2022-11-07 21:48:45 -0500

    Modified folder structure
    Made some small tweaks while waiting for docker to build

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,8 +1,14 @@
-def is_auth(session):
+def is_auth(Session):
     try:
         if type(session['auth']) is bool:
             if session['auth']:
-                return True
+                if not session['admin'] and not session['booth']:
+                    return True
+                else:
+                    if session['admin']:
+                        return 2
+                    else:
+                        return 3
             else:
                 return False
         else:
